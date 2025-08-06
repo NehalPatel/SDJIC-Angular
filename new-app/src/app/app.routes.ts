9096cd167f9739e27983ad22310f0d7d0115f521
Nehal Patel <iamnehalpatel@gmail.com>
2025-08-06 09:42:05 +0530

    first project

---
+++ new-app/src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { Courses } from './courses/courses';
+import { Home } from './home/home';
+import { Resources } from './resources/resources';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, //redirect to home page
+  { path: 'home', component: Home }, //home page
+  { path: 'courses', component: Courses }, //courses page
+  { path: 'resources', component: Resources }, //resources page
+  { path: 'about', component: About }, //about page
+  { path: 'contact', component: Contact }, //contact page
+  { path: '**', redirectTo: '/home' } //redirect to home page if the page is not found
+];
